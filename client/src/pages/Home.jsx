ea2908c745d67a33e605840282d87d6352cf7336
Masoom Rizvi <masoom2313189@gmail.com>
2023-10-29 15:48:39 +0530

    final

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -24,12 +24,15 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:8080/api/v1/post", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+      const response = await fetch(
+        "https://ai-image-generator-yg85.onrender.com/api/v1/post",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
       if (response.ok) {
         const result = await response.json();
